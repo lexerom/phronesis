92a4800f9b2648b9f4ee829bf4108a073f492732
Alexey Sukhikh <lexerom@gmail.com>
2024-10-17 22:28:16 +0300

    Fix script path

--- client/client.js
+++ client/client.js
@@ -1,5 +1,3 @@
-document.addEventListener("DOMContentLoaded", init);
-
 document.querySelector('main #deposit-button').onsubmit = async function () {
     const deposit = await fetch("/.netlify/functions/deposit-11").then((res) => res.json());
 }
