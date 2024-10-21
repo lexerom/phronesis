900752ed024933a2bfb3c8ee68e5b024d6e6fb41
Alexey Sukhikh <lexerom@gmail.com>
2024-10-21 13:21:07 +0300

    Fix typo

--- client/client.js
+++ client/client.js
@@ -1,5 +1,6 @@
 const customerId = "cus_01J75YWQJ1MB7RDY74WT324C14";
-document.querySelector('main #deposit-button').onclick = async function () {
+document.querySelector('main #deposit-button').onclick = async function (e) {
+    e.target.disabled = true;
     const response = await fetch(
         "/.netlify/functions/deposit-11", {
             method: "POST",
@@ -19,5 +20,8 @@ document.querySelector('main #deposit-button').onclick = async function () {
             depositRequestId,
         },
         jwt: token,
+        theme: {
+            colorBackground: '#f5f5f5',
+        },
     });
 }
